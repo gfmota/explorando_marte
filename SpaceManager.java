e2d2c76afbb02ff9d55ca533477cfe6a768bd57a
gfmota <gabrielfmota134@gmail.com>
2021-11-28 13:46:40 -0300

    Add input and out of land exception handler

--- SpaceManager.java
+++ SpaceManager.java
@@ -10,26 +10,51 @@ public class SpaceManager {
 
         while (in.hasNext()) {
             String[] line = in.nextLine().split(" ");
-            SpaceProbe probe = new SpaceProbe(
-                Integer.parseInt(line[0]),
-                Integer.parseInt(line[1]),
-                line[2].charAt(0)
-            );
+            SpaceProbe probe;
+            try {
+                probe = new SpaceProbe(
+                    Integer.parseInt(line[0]),
+                    Integer.parseInt(line[1]),
+                    line[2].charAt(0)
+                );
+            } catch (Exception ex) {
+                if (ex.getMessage() == "Failed to move, out of the land.") {
+                    System.out.println("Invalid position input: out of the land.");
+                    break;
+                }
+                if (ex.getMessage().startsWith("Failed to move, there's already a space scope in ")) {
+                    System.out.println("Invalid position input: there's already a space probe in this position.");
+                    break;
+                }
+                throw ex;
+            }
 
             String commands = in.nextLine();
             for (int i = 0; i < commands.length(); i++) {
-                switch (commands.charAt(i)) {
-                    case 'M':
-                        probe.moveForward();
+                try {
+                    switch (commands.charAt(i)) {
+                        case 'M':
+                            probe.moveForward();
+                            break;
+                        case 'R':
+                            probe.turnRight();
+                            break;
+                        case 'L':
+                            probe.turnLeft();
+                            break;
+                        default:
+                            throw new RuntimeException("Invalid input.");
+                    }
+                } catch (RuntimeException ex) {
+                    if (ex.getMessage() == "Facing invalid direction.") {
+                        System.out.println("Invalid direction input");
                         break;
-                    case 'R':
-                        probe.turnRight();
+                    }
+                    if (ex.getMessage() == "Failed to move, out of the land.") {
+                        System.out.print(ex.getMessage());
                         break;
-                    case 'L':
-                        probe.turnLeft();
-                        break;
-                    default:
-                        throw new RuntimeException("Invalid input.");
+                    }
+                    throw ex;
                 }
             }
 
